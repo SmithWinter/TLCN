19f12f8fae9f1f133875c3aedb21ee8cdc1f1e18
Smith Winter <hovuminhduc@gmail.com>
2021-06-27 17:59:19 +0700

    Fix token expire time

--- ElectronicShop/backend/utils/generateToken.js
+++ ElectronicShop/backend/utils/generateToken.js
@@ -5,7 +5,7 @@ const JWT_SECRET = "mongodb-test"
 const generateToken = (id) => {
   return jwt.sign({ id }, JWT_SECRET, {
     // expire in 3 min equal to ms
-    expiresIn: 3000,
+    expiresIn: 300000,
   })
 }
 
